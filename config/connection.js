6fffe0ceb7a48378e57cb31f55f13a954d7b011b
James Tu <jmsjtu@gmail.com>
2017-11-26 23:12:32 -0800

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,26 @@
 const mysql = require('mysql');
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+	const connection = mysql.createConnection({
+	  host: "localhost",
+	  port: 3306,
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+	  // Your username
+	  user: "root",
 
-  // Your username
-  user: "root",
+	  // Your password
+	  password: "",
+	  database: "burgers_db"
+	});                 
 
-  // Your password
-  password: "",
-  database: "burgers_db"
-});                 
+	connection.connect(err => {
+	  if (err) throw err;
+	    console.log('connected');
+	});
+}
 
-connection.connect(err => {
-  if (err) throw err;
-    console.log('connected');
-});
 
 module.exports = connection;
